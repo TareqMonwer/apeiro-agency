3d1a62158d1356b57180f6ceab06100788b75219
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-29 02:49:56 +0600

    service related models added

--- apeiro_agency/core/admin.py
+++ apeiro_agency/core/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    ServiceCategory,
+    Service,
+    FeatureItem,
+    PricingPlan
+)
+
+
+@admin.register(ServiceCategory)
+class AdminServiceCategory(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'created',)
+
+
+@admin.register(Service)
+class AdminService(admin.ModelAdmin):
+    list_display = ('name', 'pricing',)
+
+
+@admin.register(FeatureItem)
+class AdminFeatureItem(admin.ModelAdmin):
+    list_display = ('feature_key', 'feature',)
+
+
+@admin.register(PricingPlan)
+class AdminPricingPlan(admin.ModelAdmin):
+    list_display = (
+        'title', 'monthly_price',
+        'annual_price', 'ontime_pay'
+    )
--- apeiro_agency/core/models.py
+++ apeiro_agency/core/models.py
@@ -1,3 +1,90 @@
-from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+class PricingPlan(TimeStampedModel):
+    title = models.CharField(max_length=50)
+    monthly_price = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+    annual_price = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+    ontime_pay = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        s = f'{self.title}, '
+        if self.monthly_price:
+            s += f'monthly: {self.monthly_price} '
+        if self.annual_price:
+            s += f'annual: {self.annual_price} '
+        if self.ontime_pay:
+            s += f'onetime: {self.ontime_pay}'
+        return s.strip()
+
+
+class FeatureItem(TimeStampedModel):
+    feature_key = models.CharField(max_length=200)
+    feature = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('feature_key',)
+
+    def __str__(self):
+        return f'{self.feature_key} : {self.feature}'
+
+
+class ServiceCategory(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['name', '-created']
+        verbose_name_plural = 'Service Categories'
+        
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        update = kwargs.get('update', False)
+        if not update:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Service(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
+    description = models.TextField(blank=True, null=True)
+    features = models.ManyToManyField(FeatureItem)
+    pricing = models.ForeignKey(
+        PricingPlan,
+        on_delete=models.CASCADE,
+        related_name='services'
+    )
+
+    class Meta:
+        ordering = ['name', '-created']
+        verbose_name_plural = 'Services'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        update = kwargs.get('update', False)
+        if not update:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
