04075b92ab044907ed46ba6896c61fc735769698
tareqmonwer <tareqmonwer137@gmail.com>
2021-02-19 08:05:26 +0600

    heroku config: Disable MAILGUN_DOMAIN settings.

--- config/settings/production.py
+++ config/settings/production.py
@@ -119,7 +119,6 @@ EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 ANYMAIL = {
     "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
-    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
     "MAILGUN_API_URL": env(
         "MAILGUN_API_URL", default="https://api.mailgun.net/v3"
     ),
